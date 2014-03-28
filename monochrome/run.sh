c5c94cd3c537f828df4a114253ac9ff1325c0ac8
James Tien <southp@zillians.com>
2014-03-28 16:04:19 +0800

    Add c_st in run.sh

--- monochrome/run.sh
+++ monochrome/run.sh
@@ -6,4 +6,10 @@ if test "$1" = "thor"
 then
     cd thor/monochrome
     thorc r test --domain=mt --args $awesome
+
+elif test "$1" = "c_st"
+then
+    ./bin/c_st $awesome
+else
+    echo "What do you want?"
 fi
