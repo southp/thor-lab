544d3e5f1d42fff97f7a1f32d7425bd39548fefe
James Tien <southp@zillians.com>
2014-04-07 15:51:22 +0800

    native_exec_cmds() helper function

--- monochrome/run.sh
+++ monochrome/run.sh
@@ -15,55 +15,66 @@ options=$@
 
 for opt in $@
 do
-    if test $opt=-g
-    then
+    case $opt in
+    -g)
+        echo "AAAA" $opt
         debug=1
-    fi
+        ;;
+    esac
 done
 
 thor_exec_cmd(){
     entry_name=$1
-    is_debug=$2
 
-    if test -z $is_debug
+    if test -z $debug
     then
-        thorc r $entry_name -g --domain=mt --args $target
-    else
         thorc r $entry_name --domain=mt --args $target
+    else
+        thorc r $entry_name -g --domain=mt --args $target
     fi
 }
 
+native_exec_cmd(){
+    bin_name=$1
+
+    if test -z $debug
+    then
+        ./bin/$bin_name $target
+    else
+        cgdb --args ./bin/$bin_name $target
+    fi
+}
 
 case $impl in
 thor_func)
     cd thor/monochrome
-    thor_exec_cmd mono_function $debug
+    thor_exec_cmd mono_function
     ;;
 
 thor_per_pixel)
     cd thor/monochrome
-    thor_exec_cmd mono_async_per_pixel $debug
+    thor_exec_cmd mono_async_per_pixel
     ;;
 
 thor_per_segment)
     cd thor/monochrome
-    thor_exec_cmd mono_async_per_segment $debug
+    thor_exec_cmd mono_async_per_segment
     ;;
 
 c_st)
-    ./bin/c_st_mono $target
+    native_exec_cmd c_st_mono
     ;;
 
 c_mt)
-    ./bin/c_mt_mono $target
+    native_exec_cmd c_mt_mono
     ;;
 
 opencv)
-    ./bin/opencv_mono $target
+    native_exec_cmd opencv_mono
     ;;
 
 cuda)
-    ./bin/cuda_mono $target
+    native_exec_cmd cuda_mono
     ;;
 
 *)
